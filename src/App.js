3c1b908ef056ca2549ae3be847fc343025d034b3
Armin <armin@gmail.com>
2022-03-01 12:47:25 +0330

    add route to app.js

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import Home from './pages/Home'
 import Rooms from './pages/Rooms'
 import SingleRoom from './pages/SingleRoom'
 import Error from './pages/Error'
+import {Routes,Route,} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Home/>
-      <Rooms/>
-      <SingleRoom/>
-      <Error/>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="rooms" element={<Rooms />} />
+      <Route path=":item" element={<SingleRoom />} />
+      <Route path="*" element={<Error />} />
+    </Routes>
   );
 }
 export default App;
