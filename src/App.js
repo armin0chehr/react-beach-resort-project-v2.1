0e36ed344535cb120ac5adfc8e995a8ab2864d98
Armin <armin@gmail.com>
2022-03-01 13:46:34 +0330

    App file is  modified for routes

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App() {
     <Navbar/>
     <Routes>
       <Route path="/" element={<Home />}/>
-      <Route path="room" element={<Rooms />} />
-      <Route path=":item" element={<SingleRoom />} />
+      <Route path="rooms" element={<Rooms />} />
+      <Route path="rooms:/slug" element={<SingleRoom />} />
       <Route path="*" element={<Error />} />
     </Routes>
     </>
