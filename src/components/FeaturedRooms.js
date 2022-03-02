50f5bb84649478b332a6e440eccd293164bdcfe6
Armin <armin@gmail.com>
2022-03-02 23:10:18 +0330

    testing is state works in FeaturedRoom by using context and debug it

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -2,10 +2,14 @@ import React from 'react'
 import {RoomContext} from '../context'
 
 export default class FeaturedRooms extends React.Component {
+static contextType = RoomContext;
+
 	render() {
+
+		console.log (this.context)
 		return (
 			<div>
-				this is FeaturedRooms
+				this is FeaturedRooms {this.context.n}
 			</div>
 		)
 	}
