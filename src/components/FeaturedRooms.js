8fb1df3bb8afa9af1d5c31d994f313e7f3c65f89
Armin <armin@gmail.com>
2022-03-02 23:04:20 +0330

    initial FeaturedRoom is created and added to home

---
+++ src/components/FeaturedRooms.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {RoomContext} from '../context'
+
+export default class FeaturedRooms extends React.Component {
+	render() {
+		return (
+			<div>
+				this is FeaturedRooms
+			</div>
+		)
+	}
+}
