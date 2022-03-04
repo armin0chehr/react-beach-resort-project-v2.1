72c6e21fb08379c0c3676ab076f5c2517690c492
Armin <armin@gmail.com>
2022-03-04 18:56:40 +0330

    Featured room is edited and consumed some states also add room and loading components

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,16 +1,29 @@
 import React from 'react'
 import {RoomContext} from '../context'
+import Title from './Title'
+import Loading from './Loading'
+import Room from './Room'
 
 export default class FeaturedRooms extends React.Component {
 static contextType = RoomContext;
 
-	render() {
+render() {
+	const {featuredRooms : rooms , loading}=(this.context)
+	const room = rooms.map((room)=>{
+		return <Room key={room.id} room= {room}/>
+	})
 
-		console.log (this.context)
-		return (
-			<div>
-				this is FeaturedRooms {this.context.n}
-			</div>
-		)
+	console.log (rooms)
+
+
+return (
+	<section className= 'featured-rooms'>
+	<Title title='Featured Rooms'/>
+	<div className='featured-rooms-center'>
+
+	{loading ? <Loading/> : room }
+	</div>
+	</section >
+	)
 	}
 }
