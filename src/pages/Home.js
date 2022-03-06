091a0f8b054d24a37f4c981c1d1cea46525aed5e
Armin <armin@gmail.com>
2022-03-06 22:41:20 +0330

    initial components = contai.. filte... list;  for Rooms are creacted

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,8 @@ import Hero from '../components/Hero'
 import Banner from '../components/Banner'
 import Services from '../components/Services'
 import FeaturedRooms from '../components/FeaturedRooms'
-
 import {Link} from 'react-router-dom'
+
 export default function Home() {
 	return (
 	<>
