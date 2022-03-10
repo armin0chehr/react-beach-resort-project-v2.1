d12a757d442e85796ef467013442f680b813929b
Armin <armin@gmail.com>
2022-03-10 10:35:58 +0330

    debug RoomsList for algorithm

--- src/components/RoomsList.js
+++ src/components/RoomsList.js
@@ -3,6 +3,13 @@ import Room from './Room'
 
 export default function RoomsList({rooms}) {
 
+if (rooms.length ===0){
+	return (
+		<div className='empty-search'> 
+			<h3>there is no room match </h3>
+		</div>
+	)
+}
 
 rooms = rooms.map((room)=>{
 		return <Room key={room.id} room= {room}/>
@@ -11,9 +18,8 @@ rooms = rooms.map((room)=>{
 
 return (
 <section className="roomslist">
-    <div className="roomslist-center">  
-        
-{rooms}
+    <div className="roomslist-center">        
+	{rooms}
     </div>
 </section>
 )
